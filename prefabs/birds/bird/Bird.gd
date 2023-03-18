cd17d9848f5160b5ea5cfc3d25a2f28541ae14de
Irving Dinh <irving.dinh@gmail.com>
2023-03-18 13:54:49 +0700

    Intiial commit (cont.)

---
+++ prefabs/birds/bird/Bird.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+
+@export var SPEED = 10
+
+
+const ABOVE_OFFSET = 64
+const BELOW_OFFSET = 128
+
+
+var screen_size: Vector2
+
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	pass
+
+
+func _process(delta):
+	move_to_mouse_point(delta)
+	pass
+
+
+func move_to_mouse_point(delta: float):
+	var dest: Vector2 = get_viewport().get_mouse_position()
+	dest.x = position.x
+	dest.y = clamp(dest.y, ABOVE_OFFSET, screen_size.y - BELOW_OFFSET)
+	
+	var vect: Vector2 = dest - position
+	position = position + (vect * delta * SPEED)
